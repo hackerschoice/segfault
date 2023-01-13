5d25a90da7b6bc11d25551d13b784b5faf4237c2
SkyperTHC <skyper@thc.org>
2023-01-13 18:57:40 +0000

    fob-rc1

--- master/init-master.sh
+++ master/init-master.sh
@@ -14,7 +14,8 @@ fcgiwrap  -s "unix:${SOCKET}" -c1 -p /cgi-bin/rpc 2>&1 &
 
 # su -s /bin/bash -c "fcgiwrap  -s unix:/dev/shm/master/fcgiwrap.socket & disown" www-data
 sleep 1
-chgrp www-data "${SOCKET}"
+chgrp 33 "${SOCKET}" # Group ID from sf-rpc's www-data gid.
+# chgrp 101 "${SOCKET}" # Group ID from sf-rpc's nginx gid.
 chmod g+rwx "${SOCKET}"
 
 
@@ -23,7 +24,6 @@ arr=($(docker inspect -f '{{.Id}} {{.State.Pid}}' "sf-wg"))
 [[ ${#arr[@]} -eq 0 ]] && ERREXIT 255 "Cant get sf-wg info: res=${#arr[@]}"
 echo "WG_CID=\"${arr[0]}\"
 WG_PID=\"${arr[1]}\"" >/dev/shm/config.txt
-# echo "${SF_WG_PRIVATE}" >/dev/shm/private
 
 exec -a '[master] sleep' sleep infinity
 # sleep infinity

d1fadfcbd2f83358d0a10eef3f73b24d42817a74
SkyperTHC <skyper@thc.org>
2023-03-24 08:21:47 +0000

    cleaner

--- host/setup.sh
+++ host/setup.sh
@@ -2,7 +2,8 @@
 
 
 # Fixing vmbox permissions
-chmod 755 /etc /usr /bin
+chmod 755 /etc /usr /bin /bin/segfaultsh /bin/webshellsh
+chmod 644 /etc/english.txt
 
 echo -e "/bin/segfaultsh\n/bin/webshellsh" >>/etc/shells
 
